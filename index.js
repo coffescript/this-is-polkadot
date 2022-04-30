fe0bbf4b06278adcfc5e4caa1d81229c8ba3c165
Deiby Mejia <dmejia1204@gmail.com>
2022-04-29 19:57:52 -0600

    generation of wallets polkadot with format ethereum

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { mnemonicGenerate } = require('@polkadot/util-crypto');
+const { Keyring } = require('@polkadot/keyring');
+const { mnemonicValidate } = require('@polkadot/util-crypto');
 
 const connect = async () => {
     const wsProvider = new WsProvider('ws://127.0.0.1:9944');
@@ -14,3 +17,9 @@ connect().then((api) => {
     console.log(error);
 }).finally(() => process.exit());
 
+const keyring = new Keyring({ type: 'ethereum' });
+const mnemonic = mnemonicGenerate();
+const account = keyring.addFromMnemonic(mnemonic);
+
+console.log(`Address: ${account.address}`);
+console.log(`Mnemonic: ${mnemonic}`);
